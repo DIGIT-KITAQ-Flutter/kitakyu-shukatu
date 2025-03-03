66600d6e58fc9b659d14aebe9eacd56e9e2fb77e
ruirui <ruikeitai23@gmail.com>
2025-03-04 02:33:11 +0900

    add ボトムナビゲーションでのシーン分け

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'app.dart';
+import 'package:kitakyushu_shukatu/ui/Home/home_page.dart';
+import 'package:kitakyushu_shukatu/ui/Company/company_list_page.dart';
+import 'package:kitakyushu_shukatu/ui/MyPage/my_page.dart';
+import 'package:kitakyushu_shukatu/ui/favorite/favorites_page.dart';
+import 'package:kitakyushu_shukatu/ui/search/search_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomePage(),
+    const CompanyListPage(),
+    const FavoritesPage(),
+    const SearchPage(),
+    const MyPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "ホーム"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "検索"),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "企業一覧"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "お気に入り"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "マイページ"),
+        ],
+      ),
+    );
+  }
 }
--- lib/ui/MyPage/my_page.dart
+++ lib/ui/MyPage/my_page.dart
@@ -1,45 +1,12 @@
 import 'package:flutter/material.dart';
 
-class MyPage extends StatefulWidget {
-  const MyPage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyPage> createState() => _MyPageState();
-}
-
-class _MyPageState extends State<MyPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: Center(child: Text("マイページ")),
     );
   }
 }
